2a23d85ee6221cf14b5d7d9daa81269f2f4ad704
Pavel Nikiforov <velfor@tut.by>
2023-12-20 20:56:50 +0300

    Added a form for entering patient data
    Added a form for entering patient data into the database. Added GET request into controller to show form,  POST request into controller to save data

--- hospital/src/main/java/by/itstep/hospital/controller/PatientController.java
+++ hospital/src/main/java/by/itstep/hospital/controller/PatientController.java
@@ -7,7 +7,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -15,6 +18,11 @@ import java.util.List;
 public class PatientController {
 
     private PatientServiceImpl patientService;
+
+    @ModelAttribute("allDiagnosis")
+    public List<String> addDiagnosisToModel(){
+        return Arrays.asList("Пневмония", "Атипичная пневмония", "Ангина");
+    }
 
     @GetMapping("/")
     public String showMenu(){
@@ -24,7 +32,18 @@ public class PatientController {
     @GetMapping("/patients")
     public String showPatientsList(Model model){
         Iterable<Patient> patientsList = patientService.findAll();
-        model.addAttribute("patientsList", patientsList);
+        model.addAttribute("patients", patientsList);
         return "patients";
+    }
+
+    @GetMapping("add_patient")
+    public String showPatientInputForm(@ModelAttribute("patient") Patient patient){
+        return "add_patient";
+    }
+
+    @PostMapping("add_patient")
+    public String addPatient(@ModelAttribute("patient") Patient patient){
+        patientService.save(patient);
+        return "add_patient"; // TODO: поменять страницу на Пациент добавлен
     }
 }
